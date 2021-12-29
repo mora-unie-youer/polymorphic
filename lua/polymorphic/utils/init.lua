f5180a1a9e59254b2b91106b0c343602c4971198
Mora Unie Youer <mora_unie_youer@riseup.net>
2021-12-29 19:34:02 +0000

    feature(utils): Implemented 'split' function

--- lua/polymorphic/utils/init.lua
+++ lua/polymorphic/utils/init.lua
@@ -11,4 +11,13 @@ function U.merge(...)
 	return vim.tbl_deep_extend('force', ...)
 end
 
+function U.split(str, sep)
+	if sep == nil then sep = '%s' end
+	local result = {}
+	for w in str:gmatch('([^' .. sep .. ']*)') do
+		if w ~= '' then table.insert(result, w) end
+	end
+	return result
+end
+
 return U
