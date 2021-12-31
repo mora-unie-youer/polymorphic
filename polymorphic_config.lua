5def6063f4209c8da1466530e43011a4a9a999d5
Mora Unie Youer <mora_unie_youer@riseup.net>
2021-12-31 22:33:57 +0000

    feature(config): Add some settings in config

--- polymorphic_config.lua
+++ polymorphic_config.lua
@@ -25,6 +25,52 @@ C.config = {
 	--  This table defines all the configurations that you can tweak to
 	-- your needs.
 	polymorphic = {
+		-- Enable swap files
+		-- @false: swap files are disabled
+		-- @true: swap files are enabled
+		-- @default = false
+		swap_files = false,
+
+		-- Undo
+		-- backup enables undo settings (undofile, undodir)
+		-- @false: disable undo features
+		-- @true: enable undo file/dirs
+		-- @default = false
+		backup = false,
+
+		-- Enable line wrapping
+		-- @false: disable line wrapping
+		-- @true: enable line wrapping
+		-- @default = false
+		line_wrap = false,
+
+		-- Enable line numbering
+		-- @false: disable line numbers
+		-- @true: enable line numbers
+		-- @default = true
+		line_number = true,
+
+		-- Set numering style
+		-- @false: show absolute line numbers
+		-- @true: show relative line numbers
+		-- @default = false
+		relative_line_number = false,
+
+		-- Enable showing current mode
+		-- @false: disable show mode
+		-- @true: enable show mode
+		-- @default = true
+		show_mode = true,
+
+		-- Indent size
+		-- @default = 2
+		indent = 2,
+
+		-- Line length maximum
+		-- Define the column to show a vertical marker
+		-- @default = 80
+		max_length = 80,
+
 		--- Logging
 		-- Set polymorphic logging level
 		-- Available options: trace, debug, info, warn, error, fatal
