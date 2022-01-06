eebf7134ac947ab9df84ba38ba0a06872c3c6bd0
Mora Unie Youer <mora_unie_youer@riseup.net>
2022-01-06 22:11:52 +0000

    feature(modules.pundle): Added `local` and `git` type

---
+++ lua/polymorphic/modules/built-in/pundle/types/git.lua
@@ -0,0 +1,30 @@
+---[[---------------------------------------------------------------------]]---
+--                     __                                 __    _            --
+--        ____  ____  / /_  ______ ___  ____  _________  / /_  (_)____       --
+--       / __ \/ __ \/ / / / / __ `__ \/ __ \/ ___/ __ \/ __ \/ / ___/       --
+--      / /_/ / /_/ / / /_/ / / / / / / /_/ / /  / /_/ / / / / / /__         --
+--     / .___/\____/_/\__, /_/ /_/ /_/\____/_/  / .___/_/ /_/_/\___/         --
+--    /_/            /____/                    /_/                           --
+--       NeoVim configuration which can make your dreams come true.          --
+---[[---------------------------------------------------------------------]]---
+--               polymorphic.modules.built-in.pundle.types.git               --
+--                         Installer for git plugins                         --
+---[[---------------------------------------------------------------------]]---
+--        Author: Mora Unie Youer <mora_unie_youer@riseup.net>               --
+--          URLs: https://github.com/mora-unie-youer/polymorphic             --
+--                https://gitlab.com/mora-unie-youer/polymorphic             --
+--                https://notabug.org/mora-unie-youer/polymorphic            --
+--       License: MIT                                                        --
+---[[---------------------------------------------------------------------]]---
+
+local T = {}
+local config = nil
+
+function T.config(_config)
+	config = _config
+end
+
+function T.setup(plugin)
+end
+
+return T
