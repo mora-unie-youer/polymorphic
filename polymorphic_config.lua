f5adb66d47afe2af658238d455e065c06937b316
Mora Unie Youer <mora_unie_youer@riseup.net>
2022-01-15 07:55:52 +0000

    style(config): Aligned text in header

--- polymorphic_config.lua
+++ polymorphic_config.lua
@@ -7,8 +7,8 @@
 --    /_/            /____/                    /_/                           --
 --       NeoVim configuration which can make your dreams come true.          --
 ---[[---------------------------------------------------------------------]]---
---                        polymorphic_config.lua                             --
---                  Polymorphic user configuration file                      --
+--                          polymorphic_config.lua                           --
+--                    Polymorphic user configuration file                    --
 ---[[---------------------------------------------------------------------]]---
 --        Author: Mora Unie Youer <mora_unie_youer@riseup.net>               --
 --          URLs: https://github.com/mora-unie-youer/polymorphic             --
