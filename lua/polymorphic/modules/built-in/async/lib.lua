4c1f909b5bdf2fb1aefb61f5cd96201231275f3e
Mora Unie Youer <mora_unie_youer@riseup.net>
2022-02-15 19:45:21 +0000

    feature(modules.async): Implemented `wrap` function

--- lua/polymorphic/modules/built-in/async/lib.lua
+++ lua/polymorphic/modules/built-in/async/lib.lua
@@ -18,6 +18,36 @@
 ---[[---------------------------------------------------------------------]]---
 
 local log = require('polymorphic.extras.logging').new({ module = 'async' })
+local unpack = unpack or table.unpack
 
 local A = {}
+
+function A.wrap(func, argc)
+	assert(type(func) == 'function', 'Expected function, got ' .. type(func))
+	assert(type(argc) == 'number',   'Expected number, got ' .. type(func))
+
+	return function(...)
+		local params = { count = select('#', ...), ... }
+
+		local function future(step)
+			if step then
+				-- If count of arguments is variadic
+				if argc == -1 then
+					table.insert(params, step)
+					params.count = params.count + 1
+				else
+					params[argc] = step
+					params.count = argc
+				end
+				-- Calling function
+				return func(unpack(params))
+			else
+				return coroutine.yield(future)
+			end
+		end
+
+		return future
+	end
+end
+
 return A
