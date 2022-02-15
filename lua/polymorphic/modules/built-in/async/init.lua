d4cde225afd42d8ef015f22bf0b05466f30478fb
Mora Unie Youer <mora_unie_youer@riseup.net>
2022-02-15 19:29:15 +0000

    feature(modules.async): Started reimplementing async API, for easier usage

---
+++ lua/polymorphic/modules/built-in/async/init.lua
@@ -0,0 +1,23 @@
+---[[---------------------------------------------------------------------]]---
+--                     __                                 __    _            --
+--        ____  ____  / /_  ______ ___  ____  _________  / /_  (_)____       --
+--       / __ \/ __ \/ / / / / __ `__ \/ __ \/ ___/ __ \/ __ \/ / ___/       --
+--      / /_/ / /_/ / / /_/ / / / / / / /_/ / /  / /_/ / / / / / /__         --
+--     / .___/\____/_/\__, /_/ /_/ /_/\____/_/  / .___/_/ /_/_/\___/         --
+--    /_/            /____/                    /_/                           --
+--       NeoVim configuration which can make your dreams come true.          --
+---[[---------------------------------------------------------------------]]---
+--                    polymorphic.modules.built-in.async                     --
+--               Attempt to provide some async to polymorphic                --
+---[[---------------------------------------------------------------------]]---
+--        Author: Mora Unie Youer <mora_unie_youer@riseup.net>               --
+--          URLs: https://github.com/mora-unie-youer/polymorphic             --
+--                https://gitlab.com/mora-unie-youer/polymorphic             --
+--                https://notabug.org/mora-unie-youer/polymorphic            --
+--       License: MIT                                                        --
+---[[---------------------------------------------------------------------]]---
+
+local log = require('polymorphic.extras.logging').new({ module = 'async' })
+
+local async = {}
+return async
